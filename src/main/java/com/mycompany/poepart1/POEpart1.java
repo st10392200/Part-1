1f258e232970760a56ee4f33cd21774f7ff4b0c2
RC_Student_lab <RC_Student_lab@10.0.0.205>
2024-09-16 18:54:56 +0000

    first commit

---
+++ src/main/java/com/mycompany/poepart1/POEpart1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poepart1;
+import java.util.Scanner;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class POEpart1 {
+    
+
+    public static void main(String[] args) {
+
+       
+           
+            
+        
+    }  
+}
